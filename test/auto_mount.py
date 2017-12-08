68c47b1149d608ac5adb6e2659d3fce4d71164da
maximeGirma <maxime.girma@hotmail.fr>
2017-12-08 11:37:03 +0100

    max modif auto_mount.py

--- test/auto_mount.py
+++ test/auto_mount.py
@@ -6,7 +6,9 @@ while error_mount == 8192:
     error_mount = os.system("sudo mount /dev/sda /mnt/usbStick")
     sleep(1)
 
-os.system("cp -f /mnt/usbStick/destinataires1.csv /home/pi/test/destinataires1.csv")
+os.system("cp -f /mnt/usbStick/destinataires.csv /home/pi/test/recipients.csv")
+os.system("cp -f /mnt/usbStick/liste_capteurs.csv /home/pi/test/sensors_list.csv")
+os.system("cp -f /mnt/usbStick/frequence_alerte.csv /home/pi/test/alert_frequence.csv")
 
 
 
