6f522ccbba9664a9a506aa37a036a3578200f22c
maximeGirma <maxime.girma@hotmail.fr>
2017-12-11 10:05:11 +0100

    max

--- src/update_sensors_list.py
+++ src/update_sensors_list.py
@@ -1,13 +1,14 @@
 def update_sensors_list(sensors_list,alert_list):
 
-    i = 0
+
 
     for item in sensors_list:
+        i = 0
         while i < len(alert_list):
 
             if item.name == alert_list[i].name:
                 item.last_alert = time.time()
                 print  ("MISE A JOUR !",item.last_alert)
             i += 1
-    
+
     return sensors_list
