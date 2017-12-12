15dd6844a27ef89b7ea2e4f33813a9e6d55bf872
caiuspupus86 <kev_wfc@hotmail.fr>
2017-12-12 13:38:39 +0100

    kev modifs notify enlever le if name_list==0 + modifs mineures

--- src/notify.py
+++ src/notify.py
@@ -15,38 +15,34 @@ from email.mime.text import MIMEText
 def notify(recipients_list, final_alert_list):
 
     name_list = []
-    
+
     for item in final_alert_list:  # on crée une liste de noms à partir de chaque objet
         name_list.append(item.name)
 
-    if len(name_list) > 0:
+    #date
+    date = datetime.datetime.now()
 
-        #date
-        date = datetime.datetime.now()
+    #gestion du mail
+    msg = MIMEMultipart()
+    msg['From'] = 'secure.stand2017@gmail.com'
+    msg['To'] = ','.join(recipients_list)
+    msg['Subject'] = 'Alerte sur votre stand'
+    message = """Bonjour,
+    le (les) capteur(s) de votre stand positionné(s) sur le(la) {}
+    s'est déclenché
+    à {}
 
-        #gestion du mail
-        msg = MIMEMultipart()
-        msg['From'] = 'secure.stand2017@gmail.com'
-        msg['To'] = ','.join(recipients_list)
-        msg['Subject'] = 'Alerte sur votre stand'
-        message = """Bonjour,
-        le (les) capteur(s) de votre stand positionné(s) sur le(la) {}
-        s'est déclenché
-        à {}
-
-        Cordialement,
-        Le systeme SecureStand
-        """.format(name_list, date)
-        msg.attach(MIMEText(message))
-        mailserver = smtplib.SMTP('smtp.gmail.com', 587)
-        mailserver.ehlo()
-        mailserver.starttls()
-        mailserver.ehlo()
-        mailserver.login('secure.Stand2017@gmail.com', 'pythonlinux')
-        mailserver.sendmail('secure.Stand2017@gmail.com', recipients_list, msg.as_string())
-        print("mail envoyé")
-        mailserver.quit()
-
-        return True
+    Cordialement,
+    Le systeme SecureStand
+    """.format(name_list, date)
+    msg.attach(MIMEText(message))
+    mailserver = smtplib.SMTP('smtp.gmail.com', 587)
+    mailserver.ehlo()
+    mailserver.starttls()
+    mailserver.ehlo()
+    mailserver.login('secure.Stand2017@gmail.com', 'pythonlinux')
+    mailserver.sendmail('secure.Stand2017@gmail.com', recipients_list, msg.as_string())
+    print("mail envoyé")
+    mailserver.quit()
 
     return True
