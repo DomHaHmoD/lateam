c584dccd1a6a57774dee7fa524e7ea9dba630c8a
domh <dominique.hathi@gmail.com>
2017-12-14 10:55:29 +0100

    dhi modify main

--- src/main.py
+++ src/main.py
@@ -16,19 +16,19 @@ Commence ensuite une boucle infinie dans laquelle on va:
 A chaque étape des sécurités viennent s'assurer de la bonne marche du système.
 '''
 
-from .auto_mount import auto_mount
+from .auto_mount import auto_mount()
 auto_mount()#on commence par importer les fichiers de configuration de la clé usb
 #import des fichiers .py
-from .blescan import bluetooth_scan
-from .alert_frequence_conf import get_alert_frequence_data
-from .recipients_conf import get_recipients_data
-from .sensors_conf import get_sensors_data
-from .filters import mac_filter
-from .filters import time_filter
-from .notify import notify
-from .update_sensors_list import update_sensors_list
-from .test_conf import test_conf_file
-from .write_historic import write_histo
+from .blescan import bluetooth_scan()
+from .alert_frequence_conf import get_alert_frequence_data()
+from .recipients_conf import get_recipients_data()
+from .sensors_conf import get_sensors_data()
+from .filters import mac_filter()
+from .filters import time_filter()
+from .notify import notify()
+from .update_sensors_list import update_sensors_list()
+from .test_conf import test_conf_file()
+from .write_historic import write_histo()
 
 
 #import des biblios
