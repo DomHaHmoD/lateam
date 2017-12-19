2885b49b51a4b9afcb8872ce418a4a8cf94fc7c3
caiuspupus86 <kev_wfc@hotmail.fr>
2017-12-19 09:23:10 +0100

    kev beautify

--- main.py
+++ main.py
@@ -47,50 +47,50 @@ import time
 
 print("LA")
 
-if test_conf_file() : #si le test renvoie vrai on peut y aller, sinon, on quitte le main
+if test_conf_file():  # si le test renvoie vrai on peut y aller, sinon, on quitte le main
     print("DEDANS")
     # Déclaration des objets utiles au programme:
-    global_list = []    #liste des appareils bluetooth scannés par le BLE
-    sensors_list = get_sensors_data()   #liste des objets capteurs déployés (sensors_conf.py)
-    alert_list = []     #liste des capteurs détectés lors du scan (blescan.py/mac_filter())
-    final_alert_list = []  #liste des capteurs d'alert_list qui ont une alerte plus ancienne que 60s (time_filter.py)
-    mail_sended = False #bool pour savoir si le mail est bien parti
-    waiting_list = []   #liste d'attente contenant les capteurs pour qui le mail n'a pas pu être envoyé
-    recipients_list = get_recipients_data()  #liste des destinataires des mails d'alerte (recipients_conf.py)
-    alert_frequence = get_alert_frequence_data()      #fréquence d'alerte choisie par l'utilisateur RECUP DANS CSV!!!!
-    wifi_conf() #charge d'éventuelles login wifi 
-#    send_statut() # éventuellement pour alerter que le systeme à bien demarré
-    notify(recipients_list, final_alert_list)
+    global_list = []    # liste des appareils bluetooth scannés par le BLE
+    sensors_list = get_sensors_data()   # liste des objets capteurs déployés (sensors_conf.py)
+    alert_list = []     # liste des capteurs détectés lors du scan (blescan.py/mac_filter())
+    final_alert_list = []  # liste des capteurs d'alert_list qui ont une alerte plus ancienne que 60s (time_filter.py)
+    mail_sended = False  # bool pour savoir si le mail est bien parti
+    waiting_list = []   # liste d'attente contenant les capteurs pour qui le mail n'a pas pu être envoyé
+    recipients_list = get_recipients_data()  # liste des destinataires des mails d'alerte (recipients_conf.py)
+    alert_frequence = get_alert_frequence_data()      # fréquence d'alerte choisie par l'utilisateur RECUP DANS CSV!!!!
 
-    while True: # A partir d'ici le programme tournera jusqu'a l'arret du rasp
+    wifi_conf()  # charge d'éventuelles login wifi
+    notify(recipients_list, final_alert_list)  # envoie d'un message de bienvenue
+
+    while True:  # A partir d'ici le programme tournera jusqu'a l'arret du rasp
         print("on est dans la boucle")
-        global_list = bluetooth_scan() #On recupere notre liste d'objets scannés
+        global_list = bluetooth_scan()  # On recupere notre liste d'objets scannés
 
-        if len(global_list) == 0: #On s'assure qu'il y a quelque chose
-            time.sleep(5) # On attend 5 sec avant de relancer le scan bluetooth
+        if len(global_list) == 0:  # On s'assure qu'il y a quelque chose
+            time.sleep(5)  # On attend 5 sec avant de relancer le scan bluetooth
             continue
 
-        alert_list = mac_filter(global_list, sensors_list) # on crée notre liste d'alerte en filtrant une premiere fois
+        alert_list = mac_filter(global_list, sensors_list)  # on crée notre liste d'alerte en filtrant une premiere fois
 
-        if len(alert_list) == 0: #si ya rien on rescanne immediatement
+        if len(alert_list) == 0:  # si ya rien on rescanne immediatement
             continue
 
-        final_alert_list = time_filter(alert_list,alert_frequence) #filtre la alert_list en fonction de la derniere alerte
+        final_alert_list = time_filter(alert_list, alert_frequence)  # filtre la alert_list en fonction de la derniere alerte
 
-        if len(final_alert_list) == 0: #si ya rien on rescanne immediatement
+        if len(final_alert_list) == 0:  # si ya rien on rescanne immediatement
             continue
 
         mail_sended = notify(recipients_list, final_alert_list)
 
         if mail_sended:
 
-            sensors_list = update_sensors_list(sensors_list, final_alert_list)#met a jour le item.last_alert
+            sensors_list = update_sensors_list(sensors_list, final_alert_list)  # met a jour le item.last_alert
 
-            write_histo(recipients_list, final_alert_list)# ecrit le nom du capteur et l'heure actuelle dans l'histo
+            write_histo(recipients_list, final_alert_list)  # ecrit le nom du capteur et l'heure actuelle dans l'histo
 
-            for sensor in waiting_list: #le mail est parti avec la waiting list, donc on peut la del
+            for sensor in waiting_list:  # le mail est parti avec la waiting list, donc on peut la del
                 del sensor
 
-        else:                           #si le mail a échoué
+        else:                           # si le mail a échoué
             for alert in final_alert_list:
-                waiting_list.append(alert)#ajout des alertes a la waiting list pour le prochain tour
+                waiting_list.append(alert)  # ajout des alertes a la waiting list pour le prochain tour
